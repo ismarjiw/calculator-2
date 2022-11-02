e94b020a0bf4a749f6bc6e28a349b5146f28544f
Ismarji Workman <ismarjiw@protonmail.com>
2022-11-02 17:12:13 -0400

    final calculator 2 function

--- calculator.py
+++ calculator.py
@@ -5,3 +5,46 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+while True:
+    input_string = input('Enter your equation: ')
+    tokens = input_string.split(' ') 
+    total = None 
+
+    if len(tokens) == 1 and 'q' in tokens:
+        break 
+
+    num1 = float(tokens[1])
+    num2 = float(tokens[2])
+
+    if len(tokens) == 3: 
+        if tokens[0] == '+':
+            total = add(num1, num2)
+        elif tokens[0] == '-':
+            total = subtract(num1, num2)
+        elif tokens[0] == '*':
+            total = multiply(num1, num2)
+        elif tokens[0] == '/':
+            total = divide(num1, num2)
+        elif tokens[0] == 'square':
+            total = square(num1, num1)
+        elif tokens[0] == 'cube':
+            total = cube(num1, num1)
+        elif tokens[0] == 'mod':
+            total = mod(num1, num2)
+        elif tokens[0] == 'pow':
+            total = power(num1, num2)
+                
+    print(total)
+
+
+
+
+
+
+
+
+
+
+
+
